56d25947cb5f71bb51633f91d2833544a9a68ac9
Wdboyes13 <willdev2025@outlook.com>
2025-06-14 09:19:01 -0700

    Updated Sat 14 Jun 2025 09:19:01 PDT

--- qemu/shutdown.c
+++ qemu/shutdown.c
@@ -4,4 +4,5 @@ __attribute__((noreturn)) void qemu_shutdown() {
     outw(0x604, 0x2000);  // Magic value to ACPI shutdown in QEMU
     outw(0xB004, 0x2000); // Backup for older versions
     triple_fault();
+    __builtin_unreachable();
 }
