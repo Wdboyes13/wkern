35b2a88467e96929f7709fd05480862363651755
Wdboyes13 <willdev2025@outlook.com>
2025-06-21 18:15:52 -0700

    Updated Sat Jun 21 18:15:52 PDT 2025

--- configure.sh
+++ configure.sh
@@ -2,47 +2,66 @@
 set -euo pipefail
 trap 'rm -f tmp.c tmp.asm tmp.o disk.img' EXIT
 
+BLUE=$'\033[0;34m'
+RED=$'\033[0;31m'
+GREEN=$'\033[0;32m'
+YELLOW=$'\033[0;33m'
+MAGENTA=$'\033[0;35m'
+CYAN=$'\033[0;36m'
+CLEAR=$'\033[0m'
+
+yes() { 
+    echo -e "\033[30;42m YES \033[0m"
+}
+no()  { 
+    echo -e "\033[30;41m NO  \033[0m]"
+}
+
 if [ -f conf.mk ] && [ "${1:-}" != "--reconfigure" ]; then
-    echo "Already configured. Run with --reconfigure to force."
+    echo "${RED}Already configured. Run with --reconfigure to force.${CLEAR}"
     exit 0
+fi
+
+if [ "${1:-}" = "--reconfigure" ]; then
+    rm conf.mk
 fi
 
 check_tool() {
     tool="$1"
-    echo "Checking for $tool"
+    echo "${BLUE}Checking for $tool${CLEAR}"
     "$tool" --version >/dev/null 2>&1
     if [ $? -ne 0 ]; then
-        echo "No"
+        no
         exit 1
     else
-        echo "Yes"
+        yes
     fi
 }
 
 check_ld_flag() {
-    echo "Checking for LD Flag \"$*\""
+    echo "${RED}Checking for LD Flag \"$*\"${CLEAR}"
     echo 'int main() { return 0; }' > tmp.c
     i686-elf-gcc -c tmp.c -o tmp.o
     if i686-elf-ld "$@" -o /dev/null tmp.o >/dev/null 2>&1; then
-        echo "Yes"
+        yes
     else
-        echo "No"
+        no
         exit 1
     fi
 }
 
 check_cc_flag() {
-    echo "Checking for CC Flag \"$*\""
+    echo "${MAGENTA}Checking for CC Flag \"$*\"${CLEAR}"
     echo "int main() { return 0; }" > tmp.c
     if i686-elf-gcc "$@" -c tmp.c -o /dev/null >/dev/null 2>&1; then
-        echo "Yes"
+        yes
     else
-        echo "No"
+        no
         exit 1
     fi
 }
 
-echo "Checking for required tools"
+echo "${CYAN}Checking for required tools${CLEAR}"
 printf "\n"
 
 check_tool "make"
@@ -55,17 +74,17 @@ check_tool "dd"
 
 printf "\n"
 
-echo "Checking if mkfs.msdos works"
+echo "${CYAN}Checking if mkfs.msdos works${CLEAR}"
 dd if=/dev/zero of=disk.img bs=1M count=64 >/dev/null 2>&1
 mkfs.msdos disk.img -F 16 >/dev/null 2>&1
 if [ $? -ne 0 ]; then
-    echo "No"
+    no
     exit 1
 else
-    echo "Yes"
+    yes
 fi
 
-echo "Checking if NASM works"
+echo "${CYAN}Checking if NASM works${CLEAR}"
 cat > tmp.asm <<EOF
 _start:
 	mov eax, 0x00
@@ -74,14 +93,14 @@ _start:
 EOF
 nasm tmp.asm -o /dev/null >/dev/null 2>&1
 if [ $? -ne 0 ]; then
-    echo "No"
+    no
     exit 1
 else
-    echo "Yes"
+    yes
 fi
 
 printf "\n"
-echo "Checking for compiler flags"
+echo "${CYAN}Checking for compiler flags${CLEAR}"
 printf "\n"
 check_cc_flag "-ffreestanding"
 check_cc_flag "-nostdlib"
@@ -93,7 +112,7 @@ check_cc_flag "-std=c99"
 check_cc_flag "-g"
 printf "\n"
 
-echo "Checking for linker flags"
+echo "${CYAN}Checking for linker flags${CLEAR}"
 printf "\n"
 check_ld_flag "-melf_i386"
 check_ld_flag "-n"
@@ -104,7 +123,7 @@ echo "CONFIGURED:=1" > conf.mk
 
 
 if [ ! -d "./objs" ]; then
-    echo "Creating build directory"
+    echo "${YELLOW}Creating build directory${CLEAR}"
     find src -type d -exec mkdir -p objs/{} \;
 fi
 
