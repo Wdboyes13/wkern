73ca524e426d477267632238da4844bf80bf70d1
Wdboyes13 <willdev2025@outlook.com>
2025-06-22 11:20:07 -0700

    Updated at: 11:20:07

--- build.py
+++ build.py
@@ -19,20 +19,20 @@ def clean():
             pass
 
 def main():
-    if os.path.isfile('./didconf'):
-        if (len(sys.argv) == 1):
+    if (len(sys.argv) == 1):
+        if os.path.isfile('./didconf'):
             build()
-        elif (sys.argv[1] == "-clean"):
+        else:
+            print("Run ./configure.sh first - or ./build.py -test !")
+    elif (sys.argv[1] == "-clean"):
             clean()
-        elif (sys.argv[1] == "-git"):
+    elif (sys.argv[1] == "-git"):
             clean()
             git()
-        elif (sys.argv[1] == "-run"):
+    elif (sys.argv[1] == "-run"):
             run()
-    elif (len(sys.argv) == 2 and sys.argv[1] == "-test"):
+    elif (sys.argv[1] == "-test"):
         subprocess.run(["./configure.sh"])
-    else:
-        print("Run ./configure.sh first - or ./build.py -test !")
 
 if __name__ == "__main__":
     main()
