7b78c64e4ef57bdd1fb105cd590ff3de3883bba3
Wdboyes13 <willdev2025@outlook.com>
2025-06-26 08:04:57 -0700

    Updated at: 08:04:57

--- loc.sh
+++ loc.sh
@@ -55,5 +55,6 @@ echo "==========================================="
 
 cat > loc.md <<EOF
 # Total Lines: $total_lines
-![Lines Pie Chart](https://quickchart.io/chart?bkg=white&c=%7B%22type%22%3A%22pie%22%2C%22data%22%3A%7B%22labels%22%3A%5B%22Assembly%22%2C%22C%22%2C%22C%20Header%22%2C%22LD%20Script%22%2C%22Python%22%2C%22Shell%20Script%22%5D%2C%22datasets%22%3A%5B%7B%22label%22%3A%22Lines%22%2C%22data%22%3A%5B$asm_lines%2C$c_lines%2C$h_lines%2C$ld_lines%2C$py_lines%2C$sh_lines%5D%7D%5D%7D%7D)
+![Lines Pie Chart](https://quickchart.io/chart?bkg=white&c=%7B%22type%22%3A%22pie%22%2C%22data%22%3A%7B%22labels%22%3A%5B%22Assembly%22%2C%22C%22%2C%22C%20Header%22%2C%22LD%20Script%22%2C%22Python%22%2C%22Shell%20Script%22%5D%2C%22datasets%22%3A%5B%7B%22label%22%3A%22Lines%22%2C%22data%22%3A%5B$asm_lines%2C$c_lines%2C$h_lines%2C$ld_lines%2C$py_lines%2C$sh_lines%5D%7D%5D%7D%7D)  
+### Thank you to [quickchart.io](https://quickchart.io) For the awesome chart
 EOF
